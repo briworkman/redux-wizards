e982ca430f70ad9e54cdf3890f14c32e4a3f3e47
Brianna Workman <brianna.workman@hotmail.com>
2020-04-17 13:27:06 -0400

    inital app setup

---
+++ wizards/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <h2>Welcome to Hogwarts!</h2>
+        <p>Meet some of our wizards!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
